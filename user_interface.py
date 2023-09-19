0b35f31ae39040c0afae8350d0ed417bdfe8968e
Markus Wallin <marwa388@student.liu.se>
2023-09-19 15:52:03 +0200

    Made a working empty user interface together with jakov (pair programming)

---
+++ user_interface.py
@@ -0,0 +1,13 @@
+from tkinter import Tk
+
+
+def initiate_gui():
+#Creates gui
+    gui = Tk()
+    gui.title("Molecular Dynamics simulations")
+    gui.geometry("800x500")
+    gui.mainloop()
+    
+    
+
+initiate_gui()
