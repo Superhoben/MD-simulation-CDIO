82ec3ac00acf17bf354a70158a5b367cf9d689f4
Markus Wallin <marwa388@student.liu.se>
2023-09-19 16:57:59 +0200

    Added a pressable button that calls a function when pressed. Also made it possible to add picture with jakov (pair programming)

--- user_interface.py
+++ user_interface.py
@@ -1,13 +1,24 @@
-from tkinter import Tk
+from tkinter import *
+from PIL import ImageTk, Image
 
+    
 
 def initiate_gui():
 #Creates gui
     gui = Tk()
     gui.title("Molecular Dynamics simulations")
     gui.geometry("800x500")
+    start_sim_button = Button(gui, text="Start Simulation", command = start_simulation)
+    start_sim_button.grid(row=0)
+    img = ImageTk.PhotoImage(Image.open("<filnamn>"))
+    panel = Label(gui, image=img)
+    #panel.pack(side = "bottom", fill="both", expand="yes")
+    panel.grid(row=0, column=3)
     gui.mainloop()
-    
-    
 
-initiate_gui()
+def start_simulation():
+    print("BANGER")
+
+
+if __name__ == "__main__":
+    initiate_gui()
