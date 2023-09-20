70811df48d65c1157cac4e6b8fe5e008475e2f37
Issa Nseir <issa.10x@hotmail.com>
2023-09-20 20:52:38 +0200

    first py script which creates the config file, as well as the created config file

---
+++ config.py
@@ -0,0 +1,35 @@
+from configparser import ConfigParser
+
+def config_file():
+    
+  """ Creat the configuration file
+
+  Args:
+      None
+
+  Returns:
+      config file with "ini" format
+  """
+
+  config = ConfigParser()
+  config['User inputs - Material ID'] = {'Structure': 'Structure'}
+
+  config['User inputs - Simulation parameter'] = {'Temperature (K)': 'Temperature K',
+                      'StepsNumber': 'StepsNumber',
+                      'Potential': 'Potential'}
+
+
+
+  # Write to our config file
+  with open('config.ini','w') as config_file:
+    config.write(config_file)
+    
+
+
+config_file()
+
+
+
+
+
+
