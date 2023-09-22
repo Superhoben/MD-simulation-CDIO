84c653ae8c9f607f5730138000d77fe49e406170
Alice Eriksson <alier707@student.liu.se>
2023-09-22 12:47:52 +0200

    optimize_lattice_const | Completed optimization for the lattice constant, there is some room for improvement

--- lattice_constant.py
+++ lattice_constant.py
@@ -1,52 +1,46 @@
 from ase.lattice.cubic import FaceCenteredCubic
-from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
 from ase.md.verlet import VelocityVerlet
 from ase import units
 from asap3 import EMT
 
+
 def optimize_lattice_const(atoms):
-    """ Find optimal scaling factor for the lattice constant given in the atoms object, 
-        only searches through +-5%range with a 0.01% resolution
+    """ Finds optimal scaling factor for the lattice constant given in the atoms object,
+        only searches through +-5% range with a 0.002% resolution.
 
         Args: 
             atoms(ase atoms object): The configuration to find an optimal lattice constant for
 
         Returns: 
-            lattice(float): Optimal lattice constant for the given configuration
+            best_lattice_const_scaling(float): The optimal scaling for the cell of the atoms object
     """
+    # Initialize values
+    best_lattice_const_scaling = 0
+    best_e_tot = float('inf')
+    atoms1 = atoms.copy()
+    # Maybe add ability to choose the potential?
+    atoms1.calc = EMT()
+    original_cell = atoms.cell
+    dyn = VelocityVerlet(atoms1, 5 * units.fs)
 
-    print(atoms.cell)
+    for i in range(51):
+        # +- 5% of initial size, this is chosen arbitrarily so maybe something else is better?
+        lattice_const = 0.002 * i + 0.95
+        # Perhaps it's better to not "reset" the cell and instead do atoms1.cell = atoms1.cell + 0.002*original_cell?
+        # Then of course row above must be removed and another value for atoms1.cell must be initialized before loop.
+        atoms1.cell = original_cell * lattice_const
+        # We chose this arbitrarily, perhaps other number is better?
+        dyn.run(400)
+        # Perhaps unnecessary to divide with length? However, if big cell then might lose accuracy in comparison
+        # Perhaps this should be sampled over time also?
+        e_tot = (atoms1.get_potential_energy() + atoms1.get_kinetic_energy()) / len(atoms1)
+        if e_tot < best_e_tot:
+            best_e_tot = e_tot
+            best_lattice_const_scaling = lattice_const
 
-    best_lattice_const = 0
-    best_e_tot = 1000000
-    atoms.calc = EMT()
-
-    print(atoms.cell)
-    for i in range(10):
-        # +- 5% of initial size
-        lattice_const = 0.01*i+0.95
-        atoms1 = atoms.copy()
-        atoms1.cell = atoms1.cell*lattice_const
-        atoms1.calc = EMT()
-        dyn = VelocityVerlet(atoms1, 5 * units.fs)
-        dyn.run(200)
-        epot = atoms1.get_potential_energy() / len(atoms1)
-        ekin = atoms1.get_kinetic_energy() / len(atoms1)
-        print("Etot = ")
-        print(epot+ekin)
-        
-
-    #lattice_const
-
-    #atoms1 = atoms
-    #atoms1.cell = atoms1.cell*lattice_const
-
-    print(atoms.cell)
-    return
-
-
+    return best_lattice_const_scaling
 
 
 if __name__ == "__main__":
-    optimize_lattice_const(FaceCenteredCubic(directions=[[1,0,0], [0,1,0], [1,1,1]],
-                          size=(2,2,3), symbol='Cu', pbc=(1,1,0)))
+    optimize_lattice_const(FaceCenteredCubic(directions=[[1, 0, 0], [0, 1, 0], [1, 1, 1]],
+                                             size=(2, 2, 3), symbol='Cu', pbc=(1, 1, 0)))
