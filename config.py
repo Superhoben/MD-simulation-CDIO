c2f862f4355273171e2d1dd56659343437f304fc
IssaNseir <143752025+IssaNseir@users.noreply.github.com>
2023-09-22 14:53:48 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,34 +2,21 @@ from configparser import ConfigParser
 
 def config_file():
     
-  """ Creat the configuration file
+  """ Create the configuration file
 
   Args:
       None
 
   Returns:
-      config file with "ini" format
+      None
   """
 
   config = ConfigParser()
-  config['User inputs - Material ID'] = {'Structure': 'Structure'}
-
-  config['User inputs - Simulation parameter'] = {'Temperature (K)': 'Temperature K',
-                      'StepsNumber': 'StepsNumber',
-                      'Potential': 'Potential'}
-
-
+  config['User inputs - Material ID'] = {'Structure':'Structure'}
+  config['User inputs - Simulation parameter'] = {'Temperature (K)':'Temperature K','StepsNumber':'StepsNumber','Potential':'Potential'}
 
   # Write to our config file
   with open('config.ini','w') as config_file:
     config.write(config_file)
-    
-
 
 config_file()
-
-
-
-
-
-
