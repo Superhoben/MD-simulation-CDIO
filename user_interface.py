4ff799586a025dffe094063db17515ec955ecfc8
Markus Wallin <marwa388@student.liu.se>
2023-09-26 12:59:55 +0200

    Fixed pycodestyle, and pydocstyle errors. Removed picture and made a working gui without needing to add anything. Worked with Jakov

--- user_interface.py
+++ user_interface.py
@@ -1,26 +1,28 @@
+"""This module defines the entire GUI.
+
+At the time of writing, all functionality is not implemented
+"""
+
 from tkinter import *
 from PIL import ImageTk, Image
-from ase.build import molecule
-from ase.visualize import view
-import test
 
-    
 
 def initiate_gui():
-#Creates gui
+    """Create a GUI and defines funtcionality."""
     gui = Tk()
     gui.title("Molecular Dynamics simulations")
     gui.geometry("800x500")
-    start_sim_button = Button(gui, text="Start Simulation", command = test.testa)
+
+    start_sim_button = Button(gui, text="Start Simulation",
+                              command=start_simulation)
     start_sim_button.grid(row=0)
-    img = ImageTk.PhotoImage(Image.open("<filnamn>"))
-    panel = Label(gui, image=img)
-    #panel.pack(side = "bottom", fill="both", expand="yes")
-    panel.grid(row=0, column=3)
+
     gui.mainloop()
 
+
 def start_simulation():
-    print("BANGER")
+    """Start the simulation by calling another function."""
+    print("Test Simulation_button")
 
 
 if __name__ == "__main__":
