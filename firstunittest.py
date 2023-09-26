c5d68dc367510e81ccdf2599f1f7ff269b182fd0
IssaNseir <143752025+IssaNseir@users.noreply.github.com>
2023-09-26 12:24:23 +0200

    Update firstunittest.py
    alright since you didn't work yet on pycodestyle and pydocstyle and you are waiting for the feedback then i went and fix this code perfectly with pycodestyle, however pydocstyle need to add function description which we will do it later, else all good :)

--- firstunittest.py
+++ firstunittest.py
@@ -1,17 +1,22 @@
-"""Example functions"""
+"""Example functions."""
 
 
 def test1():
+    """Write the fucntion def."""
     return 1
 
+
 def calculate_fibonacci_number(n):
-    """Calculates the fibonacci number on index n. Index starts at 0 with value 0."""
+    """Calculates the fibonacci number on index n. Index starts
+        at 0 with value 0."""
     
     result = 0
     if n == 0:
         return 0
     elif n == 1:
-        return 1 
+        return 1
     else:
-        result = calculate_fibonacci_number(n-1) + calculate_fibonacci_number(n-2)
+        result = calculate_fibonacci_number(n-1)
+        + calculate_fibonacci_number(n-2)
     return result
+    
