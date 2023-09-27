d418ca12a61971d824cc56bfb61459e826a49403
Issa Nseir <issa.10x@hotmail.com>
2023-09-27 18:00:11 +0200

    created a config script that takes the user input and creates the config file

--- interface.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to MD simulation Software \nPlease provide the program with the parameters and conditions needed \n")
-
-print("What is the Material ID?  (hint: mp-123456)  ")
-structure = input()
