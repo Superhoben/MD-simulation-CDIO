7fab4e639990a8463bbec5369bab60245b37f351
Issa Nseir <issa.10x@hotmail.com>
2023-11-10 11:27:29 +0100

    Created a script that force the user to enter its API key

---
+++ User_API_key/start_program.py
@@ -0,0 +1,61 @@
+"""This handel the user's API key."""
+import os, sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+from configparser import ConfigParser
+
+
+# Configuration settings
+config_file = "User_API_key.ini"
+section = "User"
+
+# Create and load the configuration file
+config = ConfigParser()
+
+# Function to prompt the user for the API key
+def prompt_for_api_key():
+    while True:
+        api_key = input("Enter your API key (32 Character): ").strip()
+        if len(api_key) == 32:
+            # Save the API key in the configuration file for future use
+            config[section] = {"api_key": api_key}
+            path = os.path.dirname(os.path.abspath(__file__)) + '/../User_API_key/'
+            with open(path+config_file, "w") as configfile:
+                config.write(configfile)
+            return api_key
+        else:
+            print("Vaild key is required.")
+
+# Function to get user's API key
+def get_api_key():
+    # Check if the API key is already saved in the configuration file
+    path = os.path.dirname(os.path.abspath(__file__)) + '/../User_API_key/'
+    full_path = path+config_file
+
+    #config.read(config_file_path+config_file)
+    if os.path.isfile(full_path):
+        config.read(full_path)
+        if section in config and "api_key" in config[section]:
+            saved_api_key = config[section]["api_key"]
+            confirmation = input(f"Is this your API key? (yes/no): {saved_api_key} ").strip().lower()
+            if confirmation == "yes" or confirmation == "Yes" or confirmation == "YES":
+                print(f"Using saved API key: {saved_api_key}")
+                return saved_api_key
+            elif confirmation == "no" or confirmation == "No" or confirmation == "NO":
+                print("API key not confirmed.")
+                return prompt_for_api_key()
+            else:
+                print("Invalid option, try again")
+    
+    return prompt_for_api_key()
+
+
+
+
+if __name__ == "__main__":
+    api_key = get_api_key()
+    print(f"Using API key: {api_key}")
+#Issa's key : t4XwMQ3LLvLcnugLQQCCCII6BG85APG8
+#Gustav's key : Aumz0uNirwQYwJgWgrLVFq3Fr1Z4SfwK
+
+
+
