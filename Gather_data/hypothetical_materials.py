fcedeac49ad4ba2a3635a8a7596ea29227c5311a
Alice Eriksson <alier707@student.liu.se>
2023-11-20 13:44:57 +0100

    Made the mix of materials completely random

--- Gather_data/hypothetical_materials.py
+++ Gather_data/hypothetical_materials.py
@@ -1,8 +1,9 @@
 """This is for making hypothetical materials."""
-from ase import Atoms
 import os
-import os.path
 import sys
+import random
+import os.path
+from ase import Atoms
 from ase.io.trajectory import Trajectory
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
 
@@ -43,10 +44,36 @@ def mix_materials(traj_name: str, add_element: str, interval=10):
     traj_name = traj_path + traj_name.split(".")[0] + "_" + add_element + "_"
     for x in range(1, nr_of_atoms, interval):
         atoms_positions = original_symbols.copy()
-        interval = nr_of_atoms/x
-        for y in range(x):
-            atoms_positions[int(y*interval)] = add_element
+        random_mix(atoms_positions, add_element, x)
+        print(atoms_positions)
         atoms.set_chemical_symbols(atoms_positions)
         # TODO: add scaling = optimize_scaling(atoms) here
         traj = Trajectory(traj_name+str(x)+".traj", "w")
         traj.write(atoms)
+
+
+def random_mix(chemical_symbols: list, add_element: str, amount: int):
+    """Switch a given number of instances from a given list to a given string.
+
+    This can be used to create 'alloys': use atoms.get_chemical_symbols to
+    get the chemical symbols of the atoms object, then send this into this
+    function together with the element to mix with, and how many to add.
+    Then use atoms.set_chemical_symbols to set the new chemical symbols.
+
+    For this to make sense, the added element needs to be similar to the
+    host material in terms of structure and size.
+
+    Args:
+        chemical_symbols(list[str]): List of chemical symbols.
+        add_element(str): The element to add to the list.
+        amount(int): The amount of elements to add to the list.
+
+    Returns:
+        chemical_symbols(list[str]): The new list with randomly inserted
+            elements.
+    """
+    insert_indexes = random.sample(range(0, len(chemical_symbols)), amount)
+    for index in insert_indexes:
+        chemical_symbols[index] = add_element
+
+    return chemical_symbols
