acfb685363a50712822d2aab2c380d00650277fc
Alice Eriksson <alier707@student.liu.se>
2023-12-08 16:22:42 +0100

    Fixed minor mistake (again)

--- Simulation/make_and_run_alloys.py
+++ Simulation/make_and_run_alloys.py
@@ -7,4 +7,4 @@ from Simulation.run_md_simulation import high_throughput_mix_and_simulate
 if len(sys.argv) <= 5:
     raise Exception("Not enough arguments. Config file name, trajectory file directory, element to mix in, mixing concentrations and output directory name needs to be specified.")    
 
-high_throughput_mix_and_simulate(sys.argv[1], sys.argv[2], sys.argv[3], [float(i) for i in sys.argv[4:-1]], sys.argv[-1]):
+high_throughput_mix_and_simulate(sys.argv[1], sys.argv[2], sys.argv[3], [float(i) for i in sys.argv[4:-1]], sys.argv[-1])
